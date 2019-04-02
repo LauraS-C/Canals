5b59ea1c0c66c48e05a053a2b5a30bef38a88349
theslosty <48560065+theslosty@users.noreply.github.com>
2019-04-03 00:16:05 +0100

    Add files via upload

---
+++ KA_Data/Attraction_Rate_Function.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 09:18:18 2019
+
+A simple rating for each attraction by summing services at the given attraction. Outputs ratings list which corresponds to names_final list 
+
+@author: sl15611
+"""
+
+import pandas
+
+canal =  pandas.read_csv('KAServicesLatLong.csv',engine='python')
+
+names= canal['ï»¿Name']
+names = names.tolist()
+
+types = canal['Service']
+types = types.tolist()
+
+postcodes = canal['Postcode']
+postcodes = postcodes.tolist()
+
+#begin with arbitrarily assigning weight to each attraction type, weights can be changed as we please
+attraction_types= {
+        "Airport":1,
+        "Diesel": 1,
+        "Electricity":1,
+        "Food Shop":1,
+        "Gas":1,
+        "Laundry":1,
+        "Marina":1,
+        "Mooring Overnight":1,
+        "Public House":1,
+        "Refuse Disposal":1,
+        "Restaurant":1,
+        "Sanitory Station":1,
+        "Self Use Pump Out":1,
+        "Sewage Disposal":1,
+        "Water Point":1,
+        "Wi Fi":1}
+
+names_final =[]
+postcodes_final = []
+
+#remove repeats        
+for i in range(len(names)):
+    if (names[i] in names_final) != True or (postcodes[i] in postcodes_final) != True:
+        names_final.append(names[i])
+        postcodes_final.append(postcodes[i])
+
+#initialize ratings
+ratings = []
+for i in range(len(names_final)):
+    ratings.append(0)
+
+#sums ratings    
+for i in range(len(names_final)):
+    for j in range(len(names)):
+        if names[j] == names_final[i] and postcodes[j] == postcodes_final[i]:
+            ratings[i] += attraction_types[types[j]]
+            
+print(ratings)
